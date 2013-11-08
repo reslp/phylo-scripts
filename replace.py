261dcfe3ed0a46ca42433dcc05596aa8d1d85b52
Philipp Resl <sinnafoch@Philipps-MacBook-Pro.local>
2013-11-07 19:37:18 -0700

    update

---
+++ replace.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# script replaces - with ? at the beginning and end of textfile
+# Version3, changed: 07.11.2013
+
+import sys
+
+Info = """
+REPLACER: Replaces - with ? at the beginning and end of fasta alignments
+Usage: replace.py input.fas > output.fas
+"""
+
+if len(sys.argv) != 2:
+	sys.stderr.write(Info)
+	quit()
+else:
+	FileName = sys.argv[1]
+#FileName =  "its_all_muscle.fas" 
+
+def replace_begin (Sequenz_0):
+	zahl = 0
+	for Base in Sequenz_0:
+		if Base == "-":
+			Sequenz_0[zahl] = "?"
+		else:
+			break
+		zahl += 1
+	return "".join(Sequenz_0)
+# end replace_begin
+
+#replace all - from the end of the sequence
+def replace_end (Sequenz_0):
+	zahl2 = -1
+	for Base in Sequenz_0:
+		if Sequenz_0[len(Sequenz_0) + zahl2] == "-":
+			Sequenz_0[len(Sequenz_0) + zahl2] = "?"	
+		else:
+			break
+		zahl2 -= 1
+	return "".join(Sequenz_0)
+#end replace_end
+
+TaxonList = []
+
+
+
+File = open(FileName, "U")
+for Line in File:
+	if Line[0] == ">":
+		TaxonList.append(Line)
+
+MaxSeq = len(TaxonList)
+SequenceList = [""] * MaxSeq
+File.seek(0)
+SeqNumber = -1	
+for Line in File:
+	if Line[0] != ">":
+		SequenceList[SeqNumber] += Line
+	else:
+		SeqNumber += 1
+SeqNumber = 0
+for Single in SequenceList:
+	SequenceList[SeqNumber] = Single.replace("\n", "")
+	SequenceList[SeqNumber] = replace_begin(list(SequenceList[SeqNumber]))
+	SequenceList[SeqNumber] = replace_end(list(SequenceList[SeqNumber]))
+	TaxonList[SeqNumber] = TaxonList[SeqNumber].replace("\n","")
+	SeqNumber += 1
+
+
+for Taxon in range(0,MaxSeq):
+	print TaxonList[Taxon]
+	print SequenceList[Taxon]
+	
+
+File.close()
+	
+
+
+						
+		
+
