328ef9f7820b56d8bac8b1930113edfa5a6caa67
reslp <resl.philipp@gmail.com>
2017-08-28 14:13:49 +0200

    update

--- get_analysis_docker.sh
+++ get_analysis_docker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "Starting Concatenator"
-[ -f ./alignments/IDs_used_for_tree.txt ] && echo "Found taxon ID file" || exit
+[ -f ./input_files/IDs_used_for_tree.txt ] && echo "Found taxon ID file" || exit
 wd=$(pwd)
-mkdir tmp
+#mkdir tmp
 
-for d in $(ls ./alignments/*.fas)
+for d in $(ls ./input_files/*.fas)
 do
 	echo "Working on file:"
 	echo $d
 	echo "...Reduce"
-	./phylo-scripts/reduce.py ./alignments/IDs_used_for_tree.txt $d > tmp/reduced_${d:21}
+	./phylo-scripts/reduce.py ./input_files/IDs_used_for_tree.txt $d > tmp/reduced_${d:21}
 	echo "...Align"
 	mafft --genafpair --maxiterate 10000 --quiet tmp/reduced_${d:21} > tmp/aligned_reduced_${d:21} 
 	rm tmp/reduced_${d:21}
@@ -19,8 +19,8 @@ do
 done
 
 echo "Create concatenated alignment"
-./phylo-scripts/concat.py ./alignments/IDs_used_for_tree.txt tmp/*.fas > concat.fas
-cp concat.fas alignments/
+./phylo-scripts/concat.py ./input_files/IDs_used_for_tree.txt tmp/*.fas > concat.fas
+cp concat.fas input_files/
 echo "Done"
 exit
 
