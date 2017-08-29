d9e81a8ff73f7b4bea1c8ad142989f7ed92450e0
reslp <resl.philipp@gmail.com>
2017-08-29 21:05:37 +0200

    update bash

--- get_analysis_docker.sh
+++ get_analysis_docker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "Starting Concatenator"
+echo
 echo "Make sure to specify the FULL path to your input folder containing single locus files and the IDs_used_for_tree file"
+echo
 echo "Example command (container is called phylo): docker run -v /home/resl/my_sequences/:/input_files/ phylo"
+echo
 echo "If you use the concat pipeline in a publication please cite:"
-echo "Philipp Resl (2015): phylo-scripts: Python scripts for phylogenetics. release v0.1. available at: http://github.com/reslp DOI: 10.5281/zenodo.15983"
-echo "Philipp Resl, Kevin Schneider, Martin Westberg, Christian Printzen, Zdeněk Palice, Göran Thor, Alan Fryday, Helmut Mayrhofer and Toby Spribille (2015) Diagnostics for a troubled backbone: testing topological hypotheses of trapelioid lichenized fungi in a large-scale phylogeny of Ostropomycetidae (Lecanoromycetes). Fungal Diversity 73: 239-258"
+echo "1) Philipp Resl (2015): phylo-scripts: Python scripts for phylogenetics. release v0.1. available at: http://github.com/reslp DOI: 10.5281/zenodo.15983"
+echo "2) Philipp Resl, Kevin Schneider, Martin Westberg, Christian Printzen, Zdeněk Palice, Göran Thor, Alan Fryday, Helmut Mayrhofer and Toby Spribille (2015) Diagnostics for a troubled backbone: testing topological hypotheses of trapelioid lichenized fungi in a large-scale phylogeny of Ostropomycetidae (Lecanoromycetes). Fungal Diversity 73: 239-258"
 
 [ -f ./input_files/IDs_used_for_tree.txt ] && echo "Found taxon ID file" || exit
 wd=$(pwd)
@@ -12,21 +15,22 @@ wd=$(pwd)
 
 for d in $(ls ./input_files/*.fas)
 do
-	echo "Working on file:"
-	echo $d
-	echo "...Reduce"
+	echo "Working on file: $d"
+	echo "Calling reduce.py"
 	./phylo-scripts/reduce.py ./input_files/IDs_used_for_tree.txt $d > tmp/reduced_${d:14}
-	echo "...Align"
+	echo "Calling mafft"
 	mafft --genafpair --maxiterate 10000 --quiet tmp/reduced_${d:14} > tmp/aligned_reduced_${d:14} 
 	rm tmp/reduced_${d:14}
-	echo "...Replace"
+	echo "Calling replace.py"
 	./phylo-scripts/replace.py tmp/aligned_reduced_${d:14} > tmp/replaced_${d:14} 
 	rm tmp/aligned_reduced_${d:14}
 done
 
-echo "Creating concatenated alignment"
+echo
+echo "Calling concat.py"
 ./phylo-scripts/concat.py ./input_files/IDs_used_for_tree.txt tmp/*.fas > concat.fas
 cp concat.fas input_files/
+echo
 echo "Concatenator is done."
 exit
 
