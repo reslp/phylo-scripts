4545e284f6d373cea2794ff071ddeeb144e7c2d4
reslp <resl.philipp@gmail.com>
2017-10-05 09:35:17 +0200

    update

--- get_analysis_docker.sh
+++ get_analysis_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "********************************************************************************************************************"
 echo
-echo "Starting Concatenator, V04102017"
+echo "Starting Concatenator, V05102017"
 echo
 echo "Make sure to specify the FULL path to your input folder containing single locus files and the IDs_used_for_tree file"
 echo
